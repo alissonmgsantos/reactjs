6e6923d6d1703def8dfeb5648bd115358667da2e
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-24 16:53:07 -0300

    Criando autenticação do usuário

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 export class Login extends Component {
+  constructor() {
+    super();
+    this.state = { msg: "" };
+  }
+  envia(event) {
+    event.preventDefault();
+    fetch("localhost:8080/api/public/login", requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          this.setState({ msg: "Não foi possível fazer o login." });
+        }
+      })
+      .then(token => {});
+  }
   render() {
     return (
       <div className="login-box">
         <h1 className="header-logo">InstaPhoto</h1>
-        <form>
-          <input type="text" />
-          <input type="password" />
+        <span>{this.state.msg}</span>
+        <form onSubmit={this.envia}>
+          <input type="text" ref={input => (this.login = input)} />
+          <input type="password" ref={input => (this.senha = input)} />
           <input type="submit" value="login" />
         </form>
       </div>
