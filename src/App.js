7ca3711bb612cfaaec79279448af59cc7b94e265
Alisson Matos <alissonmgsantos@gmail.com>
2019-09-30 16:40:08 -0300

    Começando com redux

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Timeline from "./components/Timeline";
 import TimelineStore from "./logica/TimelineStore";
+import { createStore } from "redux";
 
 const timelineStore = new TimelineStore([]);
+
+//REDUCER
+function timeline(state=[],action){
+  if(action.type === 'LISTAGEM'){
+    return action.fotos;
+  }
+  return state;
+} 
+const store = createStore(timeline);
+
 
 class App extends Component {
   render() {
@@ -11,7 +22,7 @@ class App extends Component {
       <div id="root">
         <div className="main">
           <Header />
-          <Timeline login={this.props.params.login} store={timelineStore}/>
+          <Timeline login={this.props.params.login} store={store}/>
         </div>
       </div>
     );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -12,7 +12,7 @@ export default class Timeline extends Component {
 
   componentWillMount() {
     this.props.store.subscribe(fotos => {
-      this.setState({fotos});
+      this.setState({fotos:this.props.store.getState()});
     });
   }
 
@@ -26,8 +26,11 @@ export default class Timeline extends Component {
     } else {
       urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
     }
+    const listaFixa = [{"urlPerfil":"https://s3.amazonaws.com/loa-production-23ffs35gui41a/writers/images/000/000/187/big/lincoln_abraham_WD.jpg?1458837750","loginUsuario":"alots","horario":"24/09/2019 14:32","urlFoto":"https://www.fatosdesconhecidos.com.br/wp-content/uploads/2018/02/thomas-edison-1.jpg","id":1,"likeada":false,"likers":[],"comentarios":[{"login":"alots","texto":"Agora vai","id":1},{"login":"alots","texto":"Bomba","id":2},{"login":"alots","texto":"Bomba","id":3},{"login":"alots","texto":"1","id":4},{"login":"alots","texto":"Agora vai","id":5},{"login":"alots","texto":"Agora vai","id":6},{"login":"alots","texto":"x","id":7}],"comentario":"Wow que legal!"},{"urlPerfil":"https://s3.amazonaws.com/loa-production-23ffs35gui41a/writers/images/000/000/187/big/lincoln_abraham_WD.jpg?1458837750","loginUsuario":"alots","horario":"24/09/2019 14:32","urlFoto":"https://www.investors.com/wp-content/uploads/2016/03/LSpic_Franklin_031816_pd.jpg","id":2,"likeada":false,"likers":[],"comentarios":[{"login":"alots","texto":"Agora vai","id":8}],"comentario":"Isso é bom demais!"}];
 
-    this.props.store.lista(urlPerfil);
+    this.props.store.dispatch({type:'LISTAGEM',fotos:listaFixa});
+
+    // this.props.store.lista(urlPerfil);
   }
 
   componentDidMount() {
